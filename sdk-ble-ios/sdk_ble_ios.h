84d6f71ba1f2443a3610530d8d38446f17eb2fef
cskim <cskim@zoyi.co>
2019-06-03 15:28:52 +0900

    Initial Commit

---
+++ sdk-ble-ios/sdk_ble_ios.h
@@ -0,0 +1,19 @@
+//
+//  sdk_ble_ios.h
+//  sdk-ble-ios
+//
+//  Created by ChangsoonKim on 03/06/2019.
+//  Copyright © 2019 ZOYI Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sdk_ble_ios.
+FOUNDATION_EXPORT double sdk_ble_iosVersionNumber;
+
+//! Project version string for sdk_ble_ios.
+FOUNDATION_EXPORT const unsigned char sdk_ble_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sdk_ble_ios/PublicHeader.h>
+
+
